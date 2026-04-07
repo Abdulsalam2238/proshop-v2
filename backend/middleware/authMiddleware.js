feeb44264cb9ed510d9658108617869d3c697ac0
Abdulsalam2238 <abdulsalamn06@gmail.com>
2026-04-07 22:58:59 +0100

    Update JWT authentication to use Authorization header

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,22 +2,22 @@ import jwt from 'jsonwebtoken';
 import asyncHandler from './asyncHandler.js';
 import User from '../models/userModel.js';
 
-// User must be authenticated
 const protect = asyncHandler(async (req, res, next) => {
   let token;
 
-  // Read JWT from the 'jwt' cookie
-  token = req.cookies.jwt;
-
-  if (token) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
     try {
+      token = req.headers.authorization.split(' ')[1];
+
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.userId).select('-password');
 
       next();
     } catch (error) {
-      console.error(error);
       res.status(401);
       throw new Error('Not authorized, token failed');
     }
@@ -27,7 +27,6 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-// User must be an admin
 const admin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
